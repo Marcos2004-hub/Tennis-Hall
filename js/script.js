b7e02c5339fdcb0e335ca0ceadf64bf47d959116
Marcos Antônio Ribeiro Silva Norte <marcosantoniosilvanorte@gmail.com>
2025-03-28 14:11:35 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const video = document.getElementById("intro-video");
-    video.onended = function () {
-        window.location.href = "login.html"; 
-    };
-});
